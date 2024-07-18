cd19af090a95798e4775826c73cf0d9419a7fdef
jdav892 <jadavila9@gmail.com>
2024-07-18 07:51:25 -0400

    pixel updating

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 USERNAME = "whoisthis"
 TOKEN = "anything"
-
+GRAPH_ID = "graph"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -18,7 +18,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph",
+    "id": GRAPH_ID,
     "name": "Study Graph",
     "unit": "hour",
     "type": "float",
@@ -29,5 +29,16 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#print(response.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_config = {
+    "date": "20240718",
+    "quantity": "2.0"
+}
+
+
+pixel_response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+print(pixel_response.text)

111b8f3796b5e068757396f3d012bde11f5d98c3
jdav892 <jadavila9@gmail.com>
2024-07-18 07:40:14 -0400

    hit graph api and created graph

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import requests
 
+USERNAME = "whoisthis"
+TOKEN = "anything"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "anything",
-    "username": "whoisthis",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes", 
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph",
+    "name": "Study Graph",
+    "unit": "hour",
+    "type": "float",
+    "color": "kuro"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
